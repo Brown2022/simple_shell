52425db3929d94c8410591f4fdc351df65fa921e
root <stevanion2013@gmail.com>
2022-05-14 03:02:13 -0700

    Simpleshell betty checks commit

---
+++ 0-betty.c
@@ -0,0 +1,26 @@
+/**
+ * op_add - program add the sum of two parameters
+ * @arg1: first operand
+ * @arg2: second operand
+ *
+ * Return: Sum
+ */
+int op_add(int arg1, int arg2)
+
+{
+        return (arg1 + arg2);
+}
+
+
+/**
+ * print_arg - print a string using printf
+ * @arg: The string to be printed
+ * *@arg: Pointer to string
+ */
+
+void print_arg(char *arg)
+
+{
+        printf("arg");
+}
+
